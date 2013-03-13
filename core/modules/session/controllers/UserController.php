a900bc0b0b49041be5731301b877bef19579b973
Piotr <gnysek@gmail.com>
2013-03-13 22:37:57 +0100

    - user class started

--- core/modules/session/controllers/UserController.php
+++ core/modules/session/controllers/UserController.php
@@ -5,6 +5,7 @@ class UserController extends Controller
     public function loginAction()
     {
         if ($this->request->isPost()) {
+            $user = User::model()->login($_POST['Login']['login'], $_POST['Login']['pass']);
             var_dump($_POST);
         }
         $this->render('login');
