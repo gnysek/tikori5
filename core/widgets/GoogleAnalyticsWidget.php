5c1e5aa040669dce634c53a148307313822bd647
gnysek <gnysek@gmail.com>
2013-12-11 14:07:07 +0100

    google analytics widget

---
+++ core/widgets/GoogleAnalyticsWidget.php
@@ -0,0 +1,29 @@
+<?php
+class GoogleAnalyticsWidget extends Widget
+{
+
+    public $id = 'UA-XXXXXXX-X';
+
+    public function run()
+    {
+        return <<<HTML
+<script type="text/javascript">
+	var analyticsFileTypes = [''];
+	var analyticsEventTracking = 'enabled';
+</script>
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', '{$this->id}']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+	})();
+</script>
+HTML;
+
+    }
+}
