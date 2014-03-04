6416dca3e0c4330b228fb5c90a684850fe53ff48
gnysek <gnysek@gmail.com>
2014-03-04 17:01:29 +0100

    module autload fixes

--- core/tikori/Tikori.php
+++ core/tikori/Tikori.php
@@ -244,7 +244,7 @@ class Tikori
 
             foreach (array('app' => TIKORI_ROOT, 'core' => TIKORI_FPATH) as $module => $source) {
                 foreach ($paths as $path) {
-                    $file = $source . '/' . $path . 'controllers/' . $areaName . $className . '.php';
+                    $file = $source . '/' . $module . '/' . $path . 'controllers/' . $areaName . $className . '.php';
                     if (file_exists($file)) {
                         try {
                             include_once $file;
