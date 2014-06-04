818b271cf79496b33644697962f53b238b8be1fa
gnysek <gnysek@gmail.com>
2014-06-04 17:13:54 +0200

    getting assets from catalog that cannot be accessed and printing them to browser

--- core/views/core.exception.php
+++ core/views/core.exception.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="utf-8"/>
     <title><?php echo Core::app()->cfg('appName'); ?></title>
-    <style type="text/css">
-        <?php include('core.exception.css'); ?>
-    </style>
-    <link rel="stylesheet" type="text/css" href="<?php echo Core::app()->baseUrl() ?>media/prettify.css"/>
-    <script type="text/javascript" src="<?php echo Core::app()->baseUrl() ?>media/jquery-1.8.2.min.js"></script>
-    <script type="text/javascript" src="<?php echo Core::app()->baseUrl() ?>media/prettify.js"></script>
+    <?php echo Asset::jsAsset('media/jquery-1.8.2.min.js'); ?>
+    <?php echo Asset::jsAsset('media/prettify.js'); ?>
+    <?php echo Asset::cssAsset('media/reset.css'); ?>
+    <?php echo Asset::cssAsset('media/prettify.css'); ?>
+    <?php echo Asset::cssAsset('media/core.exception.css'); ?>
 </head>
 <body onload="prettyPrint();">
 
@@ -42,6 +41,7 @@
             <?php foreach ($errors as $eid => $errorData): ?>
                 <?php foreach ($errorData['files'] as $fid => $fileData): ?>
                     <div id="file_<?php echo $eid . '-' . $fid; ?>" class="codebox">
+                        <a name="file_<?php echo $eid . '-' . $fid; ?>"></a>
                         <?php if (!empty($fileData['html'])) : ?>
                             <div><?php echo $fileData['html'] ?></div>
                         <?php endif; ?>
