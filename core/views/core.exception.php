628345db4bd7c068ad3f89a28cfe42d194e8de87
gnysek <gnysek@gmail.com>
2015-07-02 15:08:02 +0200

    changes in displaying errors

--- core/views/core.exception.php
+++ core/views/core.exception.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title><?php echo Core::app()->cfg('appName'); ?></title>
+    <title><?php echo Core::app()->cfg('appName'); ?> Application Error</title>
     <?php echo Asset::jsAsset('media/jquery-1.8.2.min.js'); ?>
     <?php echo Asset::jsAsset('media/prettify.js'); ?>
     <?php echo Asset::cssAsset('media/reset.css'); ?>
@@ -15,13 +15,14 @@
     <div id="submain">
         <div id="left">
             <?php foreach ($errors as $eid => $errorData): ?>
-                <div id="error_<?php echo $eid; ?>" class="box">
-                    <p>Looks that there is a problem here:</p>
-                    <h1><strong> <code>[<?php echo $errorId; ?>] <?php echo $errorType; ?>:</code></strong></h1>
-                    <h1><strong><?php if (count($errors) > 1): echo ($eid + 1) . '/' . count($errors); endif; ?><?php echo $errorData['message']; ?></strong></h1>
+                <?php if (count($errors) > 0): ?>
+                <div id="error_<?php echo $eid; ?>" class="box" style="background-color: white;">
+                    <h1>
+                        <strong><?php echo ($eid + 1) . '/' . count($errors); ?>: <?php echo $errorData['message']; ?></strong>
+                    </h1>
                     <code class="box-wrap"><?php echo $errorData['file']; ?>:<?php echo $errorData['line']; ?></code>
-                    <p>Call stack:</p>
                 </div>
+                <?php endif; ?>
                 <?php foreach ($errorData['files'] as $fid => $fileData): ?>
                     <div id="link_<?php echo $eid . '-' . $fid; ?>" class="box box-clickable">
                         <?php if (!empty($fileData['info']['class'])) : ?>
@@ -39,7 +40,13 @@
             <?php endforeach; ?>
         </div>
         <div id="right">
-            <h1><?php echo Core::app()->cfg('appName'); ?> Application Error</h1>
+            <div class="error-header">
+                <h3><?php echo Core::app()->cfg('appName'); ?> Application Error &bull; [<?php echo $errorId; ?>] <?php echo $errorType; ?></h3>
+
+                <h1>
+                    <strong><?php if (count($errors) > 1): echo ($eid + 1) . '/' . count($errors); endif; ?><?php echo $errorData['message']; ?></strong>
+                </h1>
+            </div>
 
             <?php foreach ($errors as $eid => $errorData): ?>
                 <?php foreach ($errorData['files'] as $fid => $fileData): ?>
@@ -51,6 +58,86 @@
                     </div>
                 <?php endforeach; ?>
             <?php endforeach; ?>
+            <div class="vars">
+                <h1>Tikori Application</h1>
+
+                <table>
+                    <tr>
+                        <td class="d-k">Locale</td>
+                        <td class="d-v">en</td>
+                    </tr>
+                </table>
+
+                <h1>Tikori Request</h1>
+
+                <table>
+                    <tr>
+                        <td class="d-k">URI</td>
+                        <td class="d-v">http://x/test.php</td>
+                    </tr>
+                    <tr>
+                        <td class="d-k">Request URI</td>
+                        <td class="d-v">/x/test.php</td>
+                    </tr>
+                    <tr>
+                        <td class="d-k">Path Info</td>
+                        <td class="d-v">/x/</td>
+                    </tr>
+                    <tr>
+                        <td class="d-k">Base path</td>
+                        <td class="d-v"><?php echo htmlspecialchars('<none>'); ?></td>
+                    </tr>
+                    <tr>
+                        <td class="d-k">Base URL</td>
+                        <td class="d-v"><?php echo htmlspecialchars('<none>'); ?></td>
+                    </tr>
+                    <tr>
+                        <td class="d-k">Script name</td>
+                        <td class="d-v"><?php echo htmlspecialchars('<none>'); ?></td>
+                    </tr>
+                    <tr>
+                        <td class="d-k">Query string</td>
+                        <td class="d-v"><?php echo htmlspecialchars('<none>'); ?></td>
+                    </tr>
+                    <tr>
+                        <td class="d-k">HTTP Method</td>
+                        <td class="d-v"><?php echo htmlspecialchars('<none>'); ?></td>
+                    </tr>
+                    <tr>
+                        <td class="d-k">Scheme</td>
+                        <td class="d-v">http</td>
+                    </tr>
+                    <tr>
+                        <td class="d-k">Port</td>
+                        <td class="d-v">80</td>
+                    </tr>
+                    <tr>
+                        <td class="d-k">Host</td>
+                        <td class="d-v">localhost</td>
+                    </tr>
+                </table>
+
+                <h1>Server/Request data</h1>
+
+                <table>
+                    <?php foreach($_SERVER as $k => $v): ?>
+                        <tr>
+                            <td class="d-k"><?php echo $k; ?></td>
+                            <td class="d-v"><?php echo $v; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+
+                <?php /*<h2>Registered handlers</h2>
+
+                <table>
+                    <tr>
+                        <td class="d-k">REMOTE_ADDR</td>
+                        <td class="d-v">localhost</td>
+                    </tr>
+                </table> */ ?>
+
+            </div>
         </div>
     </div>
 </div>
@@ -60,8 +147,8 @@
         $('.codebox').hide();
         $('.codebox').first().show();
 
-        $($('.codebox').first().attr('id').replace('file_','#link_')).addClass('active');
-        console.log($('.codebox').first().attr('id').replace('file_','#link_'));
+        $($('.codebox').first().attr('id').replace('file_', '#link_')).addClass('active');
+        console.log($('.codebox').first().attr('id').replace('file_', '#link_'));
 
         $('.box-clickable').click(function () {
             var fileId = $(this).attr('id').replace(/link_/g, '#file_');
