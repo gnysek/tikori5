5690c624f1319ee91e831370c076ed5566e1175c
gnysek <gnysek@gmail.com>
2015-08-17 14:05:45 +0200

    widgets stacking

--- core/widgets/TWidget.php
+++ core/widgets/TWidget.php
@@ -3,9 +3,39 @@
 class TWidget extends TView
 {
 
+    public static $widgetStack = array();
+
+    public static function begin($properties = array())
+    {
+        $class = get_called_class();
+        $widget = new $class();
+        $widget->setupProperties($properties);
+        $widget->init();
+
+        self::$widgetStack[] = $widget;
+
+        return $widget;
+    }
+
+    public static function end()
+    {
+        if (!empty(self::$widgetStack)) {
+            $widget = array_pop(self::$widgetStack);
+            if (get_class($widget) === get_called_class()) {
+                echo $widget->run();
+                return $widget;
+            } else {
+                throw new Exception("Expecting end() of " . get_class($widget) . ", found " . get_called_class());
+            }
+        } else {
+            throw new Exception("Unexpected " . get_called_class() . '::end() call. A matching begin() for this widget is not found.');
+        }
+    }
+
+
     public function run()
     {
-        return NULL;
+        return null;
     }
 
     public function init()
@@ -28,7 +58,7 @@ class TWidget extends TView
 
     public function __get($key)
     {
-        return (array_key_exists($key, $this->_data)) ? $this->_data[$key] : NULL;
+        return (array_key_exists($key, $this->_data)) ? $this->_data[$key] : null;
     }
 
     public function __set($key, $value)
