f946d95e2010fb4a50eb437ca292f728ec14087c
gnysek <gnysek@gmail.com>
2016-08-31 17:02:14 +0200

    fixes so default controller won't break in some cases

---
+++ core/views/_tikori.welcome.php
@@ -0,0 +1,5 @@
+<h1>Tikori 5 application</h1>
+
+<p>Hello world!</p>
+
+<p>You've properly installed Tikori 5.</p>
--- core/views/old_.layout.default.php
+++ core/views/old_.layout.default.php
@@ -5,23 +5,40 @@
     <title><?php echo (!empty($this->pageTitle)) ? $this->pageTitle : Core::app()->cfg('appName') ?></title>
     <?php echo Asset::cssAsset('media/demo.css'); ?>
 </head>
-<body><?php /* @var $this Controller */ ?>
+<body>
 <div id="layout">
     <div id="inner-layout" class="cream-bg">
+        <div style="float: right; font-size: 12px; line-height: 14px;">
+            <?php
+            /* @var $this Controller */
+            /* @var $user UserWidget */
+            $user = $this->widget('User');
+            ?>
+
+            Welcome, <strong style="<?php echo $user->color ?>"><?php echo $user->username; ?></strong>!<br/>
+            <?php if (!$user->logged): ?>
+                <?php echo Html::link('Login', $user->loginUrl); ?> &bull;
+                <?php echo Html::link('Register', $user->registerUrl); ?>
+            <?php else: ?>
+                <?php echo Html::link('Profile', $user->profileUrl); ?> &bull;
+                <?php echo Html::link('Logout', $user->logoutUrl); ?>
+            <?php endif; ?>
+        </div>
+
         <h1><?php echo Core::app()->cfg('appName'); ?></h1>
         <hr/>
         <?php
         if (!empty($this->breadcrumbs)) {
             $breadcrumbs = $this->widget(
                 'Breadcrumbs', array(
-                'links' => $this->breadcrumbs,
-            ), true
+                                    'links' => $this->breadcrumbs,
+                               ), true
             );
         } else {
             $breadcrumbs = $this->widget(
                 'Breadcrumbs', array(
-                'links' => array(Core::app()->cfg('appName')),
-            ), true
+                                    'links' => array(Core::app()->cfg('appName')),
+                               ), true
             );
         }
         echo $breadcrumbs;
